87489acad1124bc072acb7ee2db020bc4c8608d2
ehasanovic4 <ehasanovic4@etf.unsa.ba>
2022-11-27 23:10:09 +0100

    Dodana stranica O nama i Footer -Edina

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Pocetna from './pages/Pocetna';
 import Kontakt from './pages/Kontakt';
+import Onama from './pages/Onama';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Rezervisi from './pages/Rezervisi';
 
 function App() {
   return (
@@ -13,7 +15,8 @@ function App() {
       <Routes>
         <Route exact path="/" element = {<Pocetna/>}/> 
         <Route exact path="/Kontakt" element = {<Kontakt/>}/> 
-
+        <Route exact path="/Rezervisi" element = {<Rezervisi/>}/> 
+        <Route exact path="/Onama" element = {<Onama/>}/> 
       </Routes>
       <Footer />
       </Router>
